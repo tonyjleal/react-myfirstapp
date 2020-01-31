c77af4334ce4f4458fb2e794b7618b2c007105c8
tonyj <tonyjleal@gmail.com>
2020-01-31 18:19:00 +0100

    Video 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from "./components/Navigation";
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-        <Navigation title="Tasks"/>
-        <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
-=======
 import { todos } from './todos.json'
 
 class App extends Component {
@@ -21,40 +10,48 @@ class App extends Component {
       todos
     }
   }
+
   render() {
     const todos = this.state.todos.map((todo, i) => {
-      return (
-        <div className="col-md-4">
-          <div className="card mt-4">
-            <div className="card-header">
-              <h3>{ todo.title }</h3>
-              <span className="badge badge-pill badge-danger ml-2">
-                {todo.priority}
-              </span>
-            </div>
-            <div className="card-body">
-              <p>{todo.description}</p>
-            </div>
-            <div className="card-footer">
-             <mark>{todo.responsable}</mark> 
-            </div>
-          </div>
-        </div>
-      );
-    })
+        return ( 
+          <div className="col-md-4 mt-4">
+            <div className="card">
+                <div className="card-header">
+                    <h3>{todo.title}</h3>
+                    <span className="badge badge-pill badge-danger ml-2">
+                      {todo.priority}
+                    </span>
+                </div>
+                <div className="card-body">
+                  <p>{todo.description}</p>
+                  <p><mark>{todo.responsable}</mark></p>
+                </div>
+            </div> 
+          </div>     
+        )
+    });
+
     return (
       <div className="App">
-          <Navigation todos={this.state.todos} />
+          <nav className="navbar navbar-dark bg-dark">
+              <a href="/" className="text-white">
+                Tasks
+                <span className="badge badge-pill badge-light ml-2">
+                  {this.state.todos.length}
+                </span>
+              </a>
+          </nav>
           <div className="container">
             <div className="row mt-4">
               { todos }
             </div>
           </div>
+          
+
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
->>>>>>> master
 }
 
 export default App;
