7747dadcd3b20be8c6d4500b13b2a72a50df003d
tonyj <tonyjleal@gmail.com>
2020-01-31 19:52:40 +0100

    Añadida función eliminar tarea

--- src/App.js
+++ src/App.js
@@ -20,10 +20,20 @@ class App extends Component {
     });
   }
 
+  removeTask(index){
+    if(window.confirm('Are you sure you want to delete it?')) {
+      this.setState({
+          todos: this.state.todos.filter((e, i) => {
+              return i !== index
+          })
+      });
+    }
+  }
+
   render() {
     const todos = this.state.todos.map((todo, i) => {
       return (
-        <div className="col-md-4">
+        <div className="col-md-4" key={i}>
           <div className="card mt-4">
             <div className="card-header">
               <h3>{ todo.title }</h3>
@@ -33,9 +43,13 @@ class App extends Component {
             </div>
             <div className="card-body">
               <p>{todo.description}</p>
+              <mark>{todo.responsable}</mark> 
             </div>
             <div className="card-footer">
-             <mark>{todo.responsable}</mark> 
+                <button className="btn btn-danger"
+                onClick={this.removeTask.bind(this, i)}>
+                  Delete
+                </button>
             </div>
           </div>
         </div>
